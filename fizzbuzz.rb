80089e3616dec0ffd431a700278a11abfc815d31
Sean Xiong <seanxionghe@gmail.com>
2020-04-14 17:38:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(n)
-  if n % 3 == 0
-    return "Fizz"
+  if n % 15 == 0
+    return "FizzBuzz"
   elsif n % 5 == 0
     return "Buzz"
-  elsif n % 15 == 0
-    return "FizzBuzz"
+  elsif n % 3 == 0
+    return "Fizz"
   else
     return
   end
